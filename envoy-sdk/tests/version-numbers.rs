06916a0bf39896e0c606e99662a33fdd7045629b
Yaroslav Skopets <yaroslav@tetrate.io>
2020-07-05 13:46:56 +0200

    documentation

--- envoy-sdk/tests/version-numbers.rs
+++ envoy-sdk/tests/version-numbers.rs
@@ -12,8 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![doc(html_root_url = "https://docs.rs/envoy-sdk/0.0.0")]
+#[test]
+fn test_readme_deps() {
+    version_sync::assert_markdown_deps_updated!("README.md");
+}
 
-pub mod extension;
-pub mod host;
-pub mod vm;
+#[test]
+fn test_html_root_url() {
+    version_sync::assert_html_root_url_updated!("src/lib.rs");
+}
