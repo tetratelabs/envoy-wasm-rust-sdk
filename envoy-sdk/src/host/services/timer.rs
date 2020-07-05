261d358e630a881b44d0c35209af2a7d198329e0
Yaroslav Skopets <yaroslav@tetrate.io>
2020-07-05 14:01:08 +0200

    todo

--- envoy-sdk/src/host/services/timer.rs
+++ envoy-sdk/src/host/services/timer.rs
@@ -16,13 +16,10 @@ extern crate std;
 
 use std::time::Duration;
 
-use crate::extension::Result;
 use crate::host;
 
 pub trait Service {
-    fn set_tick_period(&self, period: Duration, ops: &dyn TimerOps) -> host::Result<()>;
+    fn set_tick_period(&self, period: Duration) -> host::Result<()>;
 }
 
-pub trait TimerOps {
-    fn on_tick(&mut self) -> Result<()>;
-}
+// TODO: fn on_tick(&mut self) {}
