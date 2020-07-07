33dc2118a91a823a76feef6e984886ae20cae431
Yaroslav Skopets <yaroslav@tetrate.io>
2020-07-07 17:58:55 +0200

    release: bump version

--- envoy-sdk/src/lib.rs
+++ envoy-sdk/src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![doc(html_root_url = "https://docs.rs/envoy-sdk/0.0.1")]
+#![doc(html_root_url = "https://docs.rs/envoy-sdk/0.0.2")]
 
 pub mod extension;
 pub mod host;
