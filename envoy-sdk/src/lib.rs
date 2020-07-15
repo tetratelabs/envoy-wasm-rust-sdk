6e1b022c2f6fbcb8b6587b24fdf895bbf1d3510b
Yaroslav Skopets <yaroslav@tetrate.io>
2020-07-15 19:11:04 +0200

    release: bump version

--- envoy-sdk/src/lib.rs
+++ envoy-sdk/src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![doc(html_root_url = "https://docs.rs/envoy-sdk/0.0.2")]
+#![doc(html_root_url = "https://docs.rs/envoy-sdk/0.0.3")]
 
 mod abi;
 mod common;
