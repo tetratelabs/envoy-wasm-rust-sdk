5429521a3eef5837ab347890c09d897dedc6429e
Yaroslav Skopets <yaroslav@tetrate.io>
2020-07-21 18:35:33 +0200

    release: bump version

--- envoy-sdk/src/lib.rs
+++ envoy-sdk/src/lib.rs
@@ -83,7 +83,7 @@
 //! [`HowToShareStats`]: extension/factory/trait.ExtensionFactory.html#examples
 //! [`HowToUseHttpClient`]: host/http/client/trait.HttpClient.html#examples
 
-#![doc(html_root_url = "https://docs.rs/envoy-sdk/0.0.4")]
+#![doc(html_root_url = "https://docs.rs/envoy-sdk/0.0.5")]
 
 mod abi;
 mod common;
